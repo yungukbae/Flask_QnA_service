a34f22b5fdc177647b0fdeb9ae8b90b9220d1b62
yungukbae <yungukbae@gmail.com>
2021-01-14 20:01:42 +0900

    flask basic crud fin

--- app.py
+++ app.py
@@ -33,6 +33,33 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks = tasks)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form['content']
+        
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating task'
+    else:
+        return render_template('update.html',task = task)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
